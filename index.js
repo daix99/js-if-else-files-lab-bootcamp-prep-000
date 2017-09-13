0a4b015f08b719674fe4883639d0897ebc167e19
Daix Ur <d.urnikyte@gmail.com>
2017-09-13 15:52:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
-if(name==="Susan"){
-  console.log("The name is correct")
-}
 
-}else{
-  console.log("Expected the name to be Susan")
-}
-
-var index = require("./index.js")
-console.log(index.name)
 
 // Don't worry about this
 module.exports = { name, height
+}
+if (name=== "Susan"){
+console.log("The name is correct")  
 }
